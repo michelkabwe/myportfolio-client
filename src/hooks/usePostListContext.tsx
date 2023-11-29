ed7d2126bee3ab2daa3de3220e8414025088f19d
Your Name <mikael.tallbo@webexpress.se>
2023-11-29 08:24:19 +0100

    Added useContext hook to fetch data from backend Provider imported to App.tsx to wrapp the whole app

---
+++ src/hooks/usePostListContext.tsx
@@ -0,0 +1,47 @@
+import React, { createContext, useContext, useEffect, useState, ReactNode } from 'react';
+import axios from 'axios';
+
+interface CategoriesProviderProps {
+    children: ReactNode;
+}
+
+interface Category {
+    category_id: string;
+    content: string;
+    title: string;
+    post_img: string;
+
+}
+
+//type UseContext = CategoriesProviderProps & Category
+
+const CategoriesContext = createContext<Category[]>([]);
+
+export const useCategoriesContext = () => {
+    return useContext(CategoriesContext);
+};
+
+export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children }) => {
+    const [posts, setPosts] = useState<Category[]>([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get<Category[]>('http://localhost:3001/api/posts/');
+                setPosts(response.data);
+                console.log(posts,'HOOK')
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+
+    return (
+        <CategoriesContext.Provider value={posts}>
+            {children}
+        </CategoriesContext.Provider>
+    );
+};
