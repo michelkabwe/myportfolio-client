5b0f87f9ee70e5ce06b1b0205a0e5b87d4751d91
Your Name <mikael.tallbo@webexpress.se>
2023-11-30 08:21:22 +0100

    Changed css module import name Arranged typescript error on Home route component Changed name on route paths to Projects CategoriesProvider element modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
-
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import { CategoriesProvider } from './hooks/usePostListContext';
-import Home from './components/Home'
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Projects from './pages/Projects';
+import './App.css'
+
 
 
 const App: React.FC = () => {
-
-
 
   return (
     <CategoriesProvider>
       <BrowserRouter>
         <div className="App">
+          <Header />
 
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<Home category_id={''} content={''} title={''} post_img={''} showBtn={false} closeForm={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+              throw new Error('Function not implemented.');
+            } }/> } />
+            <Route path="/Projects" element={<Projects  /> } />
           </Routes>
+          <Footer/>
 
         </div>
       </BrowserRouter>
