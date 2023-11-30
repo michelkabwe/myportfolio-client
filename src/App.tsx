333f19abb66331d378b7296d8a566aa7a9b6360f
Your Name <mikael.tallbo@webexpress.se>
2023-11-30 09:27:51 +0100

    Bootsteap imported to app

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Projects from './pages/Projects';
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 
 
