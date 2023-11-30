777ddb7c596e1cfda9a56fe72a9c12b3c9225b54
Your Name <mikael.tallbo@webexpress.se>
2023-11-30 08:48:59 +0100

    Added useLocation Chnages variabel to isProjectsPage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,18 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link , useLocation} from "react-router-dom";
 import styles from "../styles/Header.module.css";
 import NavBar from "./NavBar";
 import NavResponsive from "./NavResponsive";
 
 const Header: React.FC = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
-    const isPortfolioPage = window.location.pathname === '/Projects';
+    const location = useLocation();
+    const isProjectsPage = location.pathname === '/Projects';
 
     return (
         <header
-            className={`${styles.header} ${isPortfolioPage ? styles.about : ''}`}
-            style={{ background: isPortfolioPage ? '#350bde' : '#000000' }}
+            className={`${styles.header} ${isProjectsPage ? styles.about : ''}`}
+            style={{ background: isProjectsPage ? '#350bde' : '#000000' }}
         >
             <span className={styles.logo}>
                 {" "}
