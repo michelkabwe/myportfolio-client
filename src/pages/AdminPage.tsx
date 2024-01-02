d2a798f1d83e33cd18718bcce855426860f21d26
Your Name <mikael.tallbo@webexpress.se>
2024-01-02 15:30:00 +0100

    Updated css styling Admin page

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
-import styles from '../styles/AdminPage.module.css'
+import React, { useState } from 'react'
+import AdminNav from '../pages/Admin/AdminNav';
+import styles from '../styles/AdminPage.module.css';
+import PostPage from './Admin/PostPage';
+import Dashboard from './Admin/Dashboard';
 
-const AdminPage = () => {
+
+
+const AdminPage: React.FC = () => {
+  const [activeTab, setActiveTab] = useState('dashboard')
+
+
+const handleActiveTab = (category:any) => {
+    setActiveTab(category);
+
+}
   return (
-    <div>Admin</div>
+    <div className={styles.admin_dashboard_container}>
+      <div className={styles.admin_dashboard_wrapper}>
+        <div className={styles.admin_dashboard_col_left}>
+          <AdminNav handleActiveTab={handleActiveTab} activeTab={activeTab}/>
+
+        </div>
+        <div className={styles.admin_dashboard_col_right}>
+          <div className={styles.tab_content}>
+          {activeTab === 'dashboard' ? <Dashboard /> : ''}
+
+            {activeTab === 'posts' ? <PostPage /> : ''}
+          </div>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
