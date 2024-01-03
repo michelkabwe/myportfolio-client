c98762c51fd2f79a90861b76acb726256c25a66c
Your Name <mikael.tallbo@webexpress.se>
2024-01-03 21:21:56 +0100

    Added delete icon + handledeletepost trigger import multiple values from context hook

--- src/pages/Admin/Dashboard.tsx
+++ src/pages/Admin/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import { useCategoriesContext } from "../../hooks/usePostListContext";
 import ListGroup from 'react-bootstrap/ListGroup';
 import styles from '../../styles/Dashboard.module.css';
@@ -6,18 +6,16 @@ import { FaTrashAlt } from "react-icons/fa";
 import { FaEdit } from "react-icons/fa";
 import Table from 'react-bootstrap/Table';
 import Image from 'react-bootstrap/Image';
-
+import axios from 'axios';
 
 interface Post {
+  id: number;
   title: string;
   imageUrl: string;
 }
 
-
-
 const Dashboard: React.FC = () => {
-
-  const posts: Post[] = useCategoriesContext();
+  const { posts, handleDeletePost } = useCategoriesContext();
 
   return (
     <ListGroup as="ul" className={styles.postListContainer}>
@@ -28,24 +26,22 @@ const Dashboard: React.FC = () => {
           </tr>
         </thead>
         <tbody>
-          {posts.map((post: Post, index: number) => (
-            <tr key={index} className={styles.tableRow}>
+          {posts.map((post: Post) => (
+            <tr key={post.id} className={styles.tableRow}>
               <td><Image src={post.imageUrl} alt="Thumbnail" className={styles.dashboardImg} /></td>
               <td className={styles.titleColumn}>{post.title}</td>
               <td className={styles.iconColumn}>
                 <FaEdit />
               </td>
               <td className={styles.iconColumn}>
-                <FaTrashAlt />
+                <FaTrashAlt onClick={() => handleDeletePost(post.id)}/>
               </td>
             </tr>
           ))}
         </tbody>
-
       </Table>
     </ListGroup>
-
   )
 }
 
-export default Dashboard
+export default Dashboard;
