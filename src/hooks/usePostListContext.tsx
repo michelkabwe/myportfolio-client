87321af7526cfa783c9de69f8f41adb2e4aa90f1
Your Name <mikael.tallbo@webexpress.se>
2024-01-03 21:17:01 +0100

    Added  Delete request to usePostListContext Modified hook, Now passing  values thought contextValue

--- src/hooks/usePostListContext.tsx
+++ src/hooks/usePostListContext.tsx
@@ -6,41 +6,64 @@ interface CategoriesProviderProps {
 }
 
 interface Category {
+    id: number;
     category_id: string;
     content: string;
     title: string;
     imageUrl: string;
+}
+
+interface ContextValue {
+    posts: Category[];
+    handleDeletePost: (id: number) => void;
 
 }
 
-//type UseContext = CategoriesProviderProps & Category
+const CategoriesContext = createContext<ContextValue | undefined>(undefined);
 
-const CategoriesContext = createContext<Category[]>([]);
-
-export const useCategoriesContext = () => {
-    return useContext(CategoriesContext);
+export const useCategoriesContext = (): ContextValue => {
+    const context = useContext(CategoriesContext);
+    if (!context) {
+        throw new Error('useCategoriesContext must be used within a CategoriesProvider');
+    }
+    return context;
 };
 
 export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children }) => {
     const [posts, setPosts] = useState<Category[]>([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get<Category[]>('http://localhost:3001/api/posts/');
-                setPosts(response.data);
-                console.log(posts,'HOOK')
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+    const fetchPosts = async () => {
+        try {
+            const response = await axios.get<Category[]>('http://localhost:3001/api/posts/');
+            setPosts(response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
 
-        fetchData();
+
+
+    useEffect(() => {
+        fetchPosts();
     }, []);
 
+    const handleDeletePost = async (id: number) => {
+        try {
+            await axios.delete(`http://localhost:3001/api/posts/${id}`);
+            const updatedPosts = posts.filter(post => post.id !== id);
+             setPosts(updatedPosts);
+        } catch (error) {
+            console.error('Error deleting post:', error);
+        }
+    };
+
+    const contextValue: ContextValue = {
+        posts,
+        handleDeletePost,
+    };
 
     return (
-        <CategoriesContext.Provider value={posts}>
+        <CategoriesContext.Provider value={contextValue}>
             {children}
         </CategoriesContext.Provider>
     );
