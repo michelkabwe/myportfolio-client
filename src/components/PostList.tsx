27a9b3c6a069814c5bdcc61ef223fc07c1d6ae18
Your Name <mikael.tallbo@webexpress.se>
2024-01-17 13:57:04 +0100

    Added inline style to dashboarding class

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FaTrashAlt, FaEdit } from 'react-icons/fa';
 import Image from 'react-bootstrap/Image';
 import styles from '../styles/Post.module.css';
@@ -19,21 +19,35 @@ const Post: React.FC<PostProps> = ({
   handleDeletePost,
   handlePostClick,
 }) => {
+
+  const navigate = useNavigate();
+
+
+  const goToPost = (id: number) => {
+      navigate(`/posts/${id}`);
+    };
+
+
   return (
-    <tr key={id} className={styles.tableRow} onClick={() => handlePostClick(id)}>
+
+    <tr key={id} className={styles.tableRow} >
+
       <td>
-        <Link to={`http://localhost:3001/api/posts/${id}`}>
-          <Image src={imageUrl} alt="Thumbnail" className={styles.dashboardImg} />
-        </Link>
+        <Image src={imageUrl} alt="Thumbnail" className={styles.dashboardImg} style={{width:'25px',height:'25px'}}/>
       </td>
-      <td className={styles.titleColumn}>{title}</td>
+      <td className={styles.titleColumn} onClick={() => goToPost(id)}>
+
+          {title}
+      </td>
       <td className={styles.iconColumn}>
         <FaEdit />
       </td>
       <td className={styles.iconColumn}>
         <FaTrashAlt onClick={() => handleDeletePost(id)} />
       </td>
+
     </tr>
+
   );
 };
 
