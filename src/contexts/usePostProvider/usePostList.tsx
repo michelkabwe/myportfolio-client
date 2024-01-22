db358da4dc1b9c8d53c4a2ffb0a49c37ae8a72e7
Your Name <mikael.tallbo@webexpress.se>
2024-01-22 14:27:29 +0100

    Modified endpoints to Vercel

--- src/contexts/usePostProvider/usePostList.tsx
+++ src/contexts/usePostProvider/usePostList.tsx
@@ -45,12 +45,11 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
     const [posts, setPosts] = useState<Category[]>([]);
 
     const handlePostClick = (id: number) => {
-        return `http://myportfolio-backend-ten.vercel.app/api/posts/${id}`
+        return `http://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/posts/${id}`
     };
-
     const fetchPosts = async () => {
         try {
-            const response = await axios.get<Category[]>('https://myportfolio-backend-ten.vercel.app/api/posts/');
+            const response = await axios.get<Category[]>('https://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/posts/');
             setPosts(response.data);
         } catch (error) {
             console.error('Error fetching data:', error);
@@ -91,7 +90,7 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
                 formData.append('file', selectedFile);
             }
 
-            const response: any = await axios.post('http://myportfolio-backend-ten.vercel.app/api/upload', formData, {
+            const response: any = await axios.post('http://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/upload', formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data',
                 },
@@ -114,7 +113,7 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
             formData.append('sourceCode', sourceCode);
 
 
-            const res = await axios.post<FormData>('http://myportfolio-backend-ten.vercel.app/api/posts/', {
+            const res = await axios.post<FormData>('http://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/posts/', {
                 title, content, selectedCategory, imageUrl, liveUrl, sourceCode
 
             })
@@ -128,7 +127,7 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
 
     const handleUpdatePost = async (id: number, updatedData: Partial<Category>) => {
         try {
-            const response = await axios.put(`http://myportfolio-backend-ten.vercel.app/api/posts/${id}`, updatedData);
+            const response = await axios.put(`http://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/posts/${id}`, updatedData);
             setPosts(prevPosts =>
                 prevPosts.map(post => (post.id === id ? { ...post, ...updatedData } : post))
             );
@@ -143,7 +142,7 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
 
     const handleDeletePost = async (id: number) => {
         try {
-            await axios.delete(`http://myportfolio-backend-ten.vercel.app/api/posts/${id}`);
+            await axios.delete(`http://frontend-ovlkhg7sy-michelkabwe.vercel.app/api/posts/${id}`);
             const updatedPosts = posts.filter(post => post.id !== id);
             setPosts(updatedPosts);
         } catch (error) {
