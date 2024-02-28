f3f55c60d1bcf5f2584b746662ac66f32234e597
Your Name <mikael.tallbo@webexpress.se>
2024-02-29 00:03:41 +0100

    Singel post endpoint modified

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -32,7 +32,7 @@ const Post: React.FC<Post> = () => {
   useEffect(() => {
     const fetchPost = async () => {
       try {
-        const response = await axios.get(`https://vercel.com/michelkabwe/myportfolio-backend/api/posts/${id}`);
+        const response = await axios.get(`https://myportfolio-backend-ten.vercel.app/api/posts/${id}`);
         setPost([response.data]);
       } catch (error) {
         console.error('Error fetching post:', error);
