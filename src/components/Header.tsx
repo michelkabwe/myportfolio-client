af8c0b5881a7910bcf52a8ff76e110b8b4fc5e00
Your Name <mikael.tallbo@webexpress.se>
2024-02-28 14:07:55 +0100

    Added useNavigate Added isSinglePost varibael with pathame

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,38 @@
 import React, { useState } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "../styles/Header.module.css";
 import NavBar from "./NavBar";
 import NavResponsive from "./NavResponsive";
+import { useNavigate, useLocation } from 'react-router-dom';
 
-interface Auth {
+
+
+
+
+type Auth = {
     isLoggedIn: boolean;
     isLoggedOut: boolean;
     setIsLoggedOut: React.Dispatch<React.SetStateAction<boolean>>
     setIsLoggedIn: React.Dispatch<React.SetStateAction<boolean>>
 }
 
+
+
+
 const Header: React.FC<Auth> = ({ isLoggedIn, isLoggedOut, setIsLoggedOut, setIsLoggedIn }) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const navigate = useNavigate();
     const location = useLocation();
+
+
     const isProjectsPage = location.pathname === '/Projects';
+    const isSinglePost = location.pathname.startsWith('/api/posts/:id');
 
     return (
         <header
             className={`${styles.header} ${isProjectsPage ? styles.about : ''}`}
             style={{ background: isProjectsPage ? '#350bde' : '#000000' }}
-        >
+            >
             <span className={styles.logo}>
                 {" "}
                 <Link to="/">Michel Kabwe</Link>
