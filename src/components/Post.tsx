08cfb8b2e37cc3ba8110425f20b8515cebc363e7
Your Name <mikael.tallbo@webexpress.se>
2024-03-05 18:29:14 +0100

    Added useParams to get id

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams} from 'react-router-dom';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
 import styles from '../styles/Post.module.css';
 
 
