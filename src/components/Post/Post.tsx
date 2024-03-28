956f3c5ff9c99552a005164ebeb5e3fed133d8fb
Your Name <mikael.tallbo@webexpress.se>
2024-03-28 11:21:22 +0100

    Added CodeLanguageIcons component Removed codeLanguage code from Post.tsx and Project.tsx Imported CodeLanguageIcons component to Post.tsx and Projects.tsx Modified Posttsx styling

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -2,13 +2,9 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import styles from '../../styles/Post.module.css';
-import { FaNode } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io5";
-import { SiTypescript } from "react-icons/si";
-import { SiFirebase } from "react-icons/si";
-import { BsBootstrapFill } from "react-icons/bs";
-import { FaReact } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
+import CodeLanguageIcons from './CodeLanguageIcons';
+
+
 
 interface Post {
   title: string;
@@ -20,7 +16,7 @@ interface Post {
   liveUrl: string;
 }
 
-const Post: React.FC<Post> = ({ title, content, imageUrl, codeLangIcon, id, sourceCode, liveUrl }) => {
+const Post: React.FC<Post> = () => {
   const { id: postId } = useParams();
   const [post, setPost] = useState<Post[]>([]);
 
@@ -35,27 +31,6 @@ const Post: React.FC<Post> = ({ title, content, imageUrl, codeLangIcon, id, sour
     };
     fetchPost();
   }, [postId]);
-
-  const renderIcon = (lang: string) => {
-    switch (lang) {
-      case 'node':
-        return <FaNode size={30} color="#fcf55f" />;
-      case 'javascript':
-        return <IoLogoJavascript size={30} color="#fcf55f" />;
-      case 'typescript':
-        return <SiTypescript size={26} color="#fcf55f" />;
-      case 'firebase':
-        return <SiFirebase size={30} color="#fcf55f" />;
-      case 'bootstrap':
-        return <BsBootstrapFill size={30} color="#fcf55f" />;
-      case 'react':
-        return <FaReact size={30} color="#fcf55f" />;
-      case 'github':
-        return <FaGithub size={30} color="#fcf55f" />;
-      default:
-        return null;
-    }
-  };
 
   return (
     <>
@@ -79,16 +54,11 @@ const Post: React.FC<Post> = ({ title, content, imageUrl, codeLangIcon, id, sour
                     <a href={item.sourceCode}>Source code</a>
                   </div>
                 </div>
+                <div className={styles.codeLanguageWrapper}>
+                <CodeLanguageIcons {...item}/>
               </div>
-              <div className={styles.codeLanguageWrapper}>
-                <div className={styles.codeLanguageIcons}>
-                  {item.codeLangIcon.split(',').map((lang, index) => (
-                    <div key={index} className={styles.codeIcon}>
-                      {renderIcon(lang.trim())}
-                    </div>
-                  ))}
-                </div>
               </div>
+
             </div>
           </section>
         ))
