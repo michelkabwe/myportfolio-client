f32ef7874f2f5d8e9858e5dc1f468f848ed2722a
michelkabwe <michel.kabwe1@gmail.com>
2025-04-10 23:32:03 +0200

    End point replaced

--- src/contexts/usePostProvider/usePostList.tsx
+++ src/contexts/usePostProvider/usePostList.tsx
@@ -68,7 +68,7 @@ export const CategoriesProvider: React.FC<CategoriesProviderProps> = ({ children
 
         const fetchPosts = async () => {
             try {
-                const response = await axios.get<Category[]>('https://myportfolio-backend-ten.vercel.app/api/posts/');
+                const response = await axios.get<Category[]>('http://localhost:3001/api/posts/');
                 setPosts(response.data);
             } catch (error) {
                 console.error('Error fetching data:', error);
